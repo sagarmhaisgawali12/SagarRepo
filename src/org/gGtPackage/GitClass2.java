07cbdea82a0e1685e6b58e3415e99bb62fbf1e30
DELL <DELL@DESKTOP-7QK8N4O>
2022-07-08 15:40:45 +0530

    My Second commit.......

---
+++ src/org/gGtPackage/GitClass2.java
@@ -0,0 +1,14 @@
+package org.gGtPackage;
+
+public class GitClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=a;
+		
+		System.out.println(a);
+
+	}
+
+}
