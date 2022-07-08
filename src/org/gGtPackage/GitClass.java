a853929f05d31d190bdb5e0bc10f37d49fab50bb
DELL <DELL@DESKTOP-7QK8N4O>
2022-07-08 14:18:27 +0530

    My first commit in Git demo....

---
+++ src/org/gGtPackage/GitClass.java
@@ -0,0 +1,11 @@
+package org.gGtPackage;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sagar is my name");
+
+	}
+
+}
